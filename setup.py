5c56ce85e255a6f3362cffd4c64097ab68c06603
Tim Head <betatim@gmail.com>
2021-01-17 15:55:24 +0100

    Release 0.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(HERE, "README.md"), encoding="utf8") as f:
 setup(
     author="Tim Head",
     author_email="betatim@gmail.com",
-    version="0.3.1",
+    version="0.4.0",
     description="Jupyter extension to export notebooks as PDFs",
     install_requires=["nbconvert", "pyppeteer", "PyPDF2"],
     keywords="jupyter pdf export bundler",
